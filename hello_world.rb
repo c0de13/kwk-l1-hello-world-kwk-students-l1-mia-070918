12f4101206658dd5a3530cd5df6ea749b6dd5ae4
c0de13 <meganmpuppy@gmail.com>
2018-07-09 17:44:11 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -2,5 +2,9 @@ puts "Hello World!" #the number sign is like \\ in java (comments)
 
 print "Hello World!" #puts is like println and print is like print
 
-Double_quotes = "String"
-puts Double_quotes
+q = "String"
+puts q 
+
+q.upcase
+
+
